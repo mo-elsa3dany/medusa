5ef354fb1c1c2f69ae07ad987f836a874726f9eb
mo-elsa3dany <68140011+mo-elsa3dany@users.noreply.github.com>
2021-01-31 21:43:59 -0500

    update

--- mail.php
+++ mail.php
@@ -1,31 +1,22 @@
 <?php
-    $to = 'mohamed.elsa3dany@gmail.com';
-    $firstname = $_POST["fname"];
-    $email= $_POST["email"];
-    $text= $_POST["message"];
-    $phone= $_POST["phone"];
+$to = 'mohamed.elsa3dany@gmail.com';
+$firstname = $_POST["fname"];
+$email= $_POST["email"];
+$headers = 'MIME-Version: 1.0' . "\r\n";
+$headers .= "From: " . $email . "\r\n"; // Sender's E-mail
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
+$message ='<table style="width:100%">
+    <tbody><tr>
+        <td>'.$firstname.'  '.$laststname.'</td>
+    </tr>
+    <tr><td>Email: '.$email.'</td></tr>
+</tbody></table>';
 
-
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-    $message ='<table style="width:100%">
-        <tr>
-            <td>'.$firstname.'  '.$laststname.'</td>
-        </tr>
-        <tr><td>Email: '.$email.'</td></tr>
-        <tr><td>phone: '.$phone.'</td></tr>
-        <tr><td>Text: '.$text.'</td></tr>
-
-    </table>';
-
-    if (@mail($to, $email, $message, $headers))
-    {
-        echo 'The message has been sent.';
-    }else{
-        echo 'failed';
-    }
-
+if (@mail($to, $email, $message, $headers))
+{
+    echo 'The message has been sent.';
+}else{
+    echo 'failed';
+}
 ?>
